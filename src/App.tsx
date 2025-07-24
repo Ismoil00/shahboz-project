de00e829729cda4e1744706afa0d6c3952a5ec3f
Ismoil Sharifov <sharipovismoil01@gmail.com>
2025-07-24 22:12:49 +0500

    initiation

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="text-red-500">HELLO WORLD</div>;
+}
+
+export default App;
